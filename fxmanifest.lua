80845e6f690227c9262a1bc6ff25c9a7ca08dba4
KnoxDevelopment <142834101+KnoxDevelopment@users.noreply.github.com>
2023-08-22 01:56:26 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Knox Development'
+description 'Knox Development | Head Bag | discord.gg/W49wK6vveV'
+version '1.0.0'
+
+shared_scripts {
+    'Shared/*.lua'
+}
+
+client_scripts {
+    'Client/*.lua'
+}
+
+server_scripts {
+    'Server/*.lua'
+}
+
+ui_page 'Html/index.html'
+
+files {
+	'Html/*.*'
+}
